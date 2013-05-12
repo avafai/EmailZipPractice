88fe2bd9b2b655d46efbe86ea60d1883a9c18cc2
WECoyote <ADV@Millennium-Falcon.local>
2013-05-11 21:58:11 -0700

    Initial Commit

---
+++ EmailZipPractice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EmailZipPractice
+//
+//  Created by Ali Vafai on 5/11/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
